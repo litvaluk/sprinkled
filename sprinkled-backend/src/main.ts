8a580a050c09838350f984d2e2bca1e4acaa4541
Lukáš Litvan <lukas.litvan@gmail.com>
2022-09-17 16:47:55 +0200

    Use helmet package (security-related HTTP headers) and enable cors

--- sprinkled-backend/src/main.ts
+++ sprinkled-backend/src/main.ts
@@ -1,12 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './modules';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Sprinkled')
